2a9d19daf4111edf409427956465c58fee3e7f2d
Teunuzzz <135118766+Teunuzzz@users.noreply.github.com>
2025-11-07 14:47:39 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline cache for PWA
-const CACHE = 'wissel-planner-v1';
+const CACHE = 'wissel-planner-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -7,20 +7,16 @@ const ASSETS = [
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  const req = e.request;
   e.respondWith(
-    caches.match(req).then(res => res || fetch(req).then(fetchRes => fetchRes).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match('./index.html')))
   );
 });
