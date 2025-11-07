e3bd520d48330f5354b00a0f81d412c81b446620
Teunuzzz <135118766+Teunuzzz@users.noreply.github.com>
2025-11-07 22:59:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
-const C='wissel-planner-mobile-v2_3_14';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==C&&caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('wissel-v1').then(c=>c.addAll([
+    './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
+  ])));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
