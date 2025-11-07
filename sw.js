83cc16169ef5b6a01e16a959e6c96d8c2b5b7c44
Teunuzzz <135118766+Teunuzzz@users.noreply.github.com>
2025-11-07 16:58:03 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// Offline cache (v2_3_3)
-const CACHE = 'wissel-planner-mobile-v2_3_3';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('./index.html')))); });
