f1b1b3155b26b1d114e703ed14b70631ebfa5c86
Teunuzzz <135118766+Teunuzzz@users.noreply.github.com>
2025-11-07 23:14:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('wissel-v2').then(c=>c.addAll([
+    './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
+  ])));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
